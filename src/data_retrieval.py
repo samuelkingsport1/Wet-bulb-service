7afb72ed6c556376040618c050723e7ef4a3f710
samuelkingsport1 <57406216+samuelkingsport1@users.noreply.github.com>
2023-07-08 14:37:39 +0000

    accounted for zip file

--- src/data_retrieval.py
+++ src/data_retrieval.py
@@ -1,16 +1,14 @@
 import requests
 import pandas as pd
-import io
 import zipfile
+import io
 
 def retrieve_weather_data(api_key):
-    url = f"http://api.climately.com/v1/{api_key}/weather/hourly/all.zip"
+    url = f"http://api.climately.com/v1/{api_key}/current/all.zip"
     response = requests.get(url)
-    
-    # Check if the request was successful
+
     if response.status_code == 200:
         # Assuming the response is in ZIP format, extract the CSV file
-        # and load it into a dataframe
         with zipfile.ZipFile(io.BytesIO(response.content)) as z:
             csv_filename = z.namelist()[0]
             with z.open(csv_filename) as f:
@@ -21,6 +19,5 @@ def retrieve_weather_data(api_key):
         return df
     
     else:
-        # Handle the case when the request fails
         print("Error retrieving weather data:", response.status_code)
         return None
